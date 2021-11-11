058063a5ffb66e42f8f59955a2b4e4b705cf1370
johnnygeee <94137832+johnnygeee@users.noreply.github.com>
2021-11-11 13:57:47 -0500

    Add files via upload
    Added python textbook file

---
+++ cities.py
@@ -0,0 +1,9 @@
+prompt = "Please enter the name of a city you have visited: "
+prompt += "\n(Enter 'quit' when you are finished): "
+
+while True:
+	city = input(prompt)
+	if city == 'quit':
+		break
+	else:
+		print(f"I'd really love to go to {city.title()}!")
